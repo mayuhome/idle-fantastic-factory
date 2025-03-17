88dbaea352c53df77a36a03d7c5535685bb355c3
jade <mayuhome@163.com>
2025-03-17 22:39:25 +0800

    init & add tailwind

---
+++ src/app/pages/factory/factory.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-factory',
+  imports: [],
+  templateUrl: './factory.component.html',
+  styleUrl: './factory.component.css',
+})
+export class FactoryComponent {
+
+}
